c314771573a7a7ff53257fcca4f1e0c47c9bb7b5
githyj-jang <hoo9268@gmail.com>
2023-12-15 08:02:05 +0900

    reorder

--- app/src/main/java/com/example/android_project/Calendar.java
+++ app/src/main/java/com/example/android_project/Calendar.java
@@ -16,50 +16,7 @@ import java.util.Locale;
 
 public class Calendar extends AppCompatActivity {
     private DatabaseHelper db;
-    public Show makeSample(int i) {
-        Show show = new Show();
-        String k = String.valueOf(i);
-        show.title = "title" +k;
-        show.type = "type";
-        if (i %4== 1){
-            show.period = "20231213 ~ 20231213";
-        } else if (i%4==2) {
-            show.period = "20231216 ~ 20231217";
-        } else if (i%4==3) {
-            show.period = "20231215 ~ 20231215";
-        }else{
-            show.period = "20231219 ~ 20231219";
-        }
-        show.eventPeriod = "eventPeriod";
-        show.eventSite = "eventSite";
-        show.charge = "charge";
-        show.contactPoint = "contactPoint";
-        show.url ="https://www.mcst.go.kr/kor/s_culture/culture/cultureView.jsp?pSeq=40770";
-        if (i %10 == 1){
-            show.imageObject = "https://www.mcst.go.kr/attachFiles/cultureInfoCourt/monthServ/1702513157312.jpg";
-        } else if (i %10==2) {
-            show.imageObject = "https://www.mcst.go.kr/attachFiles/cultureInfoCourt/monthServ/1702442615283.jpg";
-        } else if (i %10==3) {
-            show.imageObject = "https://www.mcst.go.kr/attachFiles/cultureInfoCourt/monthServ/1702427175230.jpg";
-        } else if (i %10==4){
-            show.imageObject ="https://www.mcst.go.kr/attachFiles/cultureInfoCourt/monthServ/1702514099694.gif";
-        } else if (i %10==5){
-            show.imageObject ="https://www.mcst.go.kr/attachFiles/cultureInfoCourt/monthServ/1702514134298.jpg";
-        } else if (i %10==6){
-            show.imageObject ="https://www.mcst.go.kr/attachFiles/cultureInfoCourt/monthServ/1699417551991.jpg";
-        } else if (i %10==7){
-            show.imageObject ="https://www.mcst.go.kr/attachFiles/cultureInfoCourt/monthServ/1701653101315.jpg";
-        } else if (i %10==8){
-            show.imageObject ="https://www.mcst.go.kr/attachFiles/cultureInfoCourt/monthServ/1701739949828.jpg";
-        } else if(i %10==9){
-            show.imageObject ="https://www.mcst.go.kr/attachFiles/cultureInfoCourt/monthServ/1702267476272.jpg";
-        } else{
-            show.imageObject ="https://www.mcst.go.kr/attachFiles/cultureInfoCourt/monthServ/1701848885467.jpg";
-        }
-        show.description = "description";
-        show.viewCount = "viewCount";
-        return show;
-    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -112,6 +69,50 @@ public class Calendar extends AppCompatActivity {
                         .commit();
             }
         });
+    }
+    public Show makeSample(int i) {
+        Show show = new Show();
+        String k = String.valueOf(i);
+        show.title = "title" +k;
+        show.type = "type";
+        if (i %4== 1){
+            show.period = "20231213 ~ 20231213";
+        } else if (i%4==2) {
+            show.period = "20231216 ~ 20231217";
+        } else if (i%4==3) {
+            show.period = "20231215 ~ 20231215";
+        }else{
+            show.period = "20231219 ~ 20231219";
+        }
+        show.eventPeriod = "eventPeriod";
+        show.eventSite = "eventSite";
+        show.charge = "charge";
+        show.contactPoint = "contactPoint";
+        show.url ="https://www.mcst.go.kr/kor/s_culture/culture/cultureView.jsp?pSeq=40770";
+        if (i %10 == 1){
+            show.imageObject = "https://www.mcst.go.kr/attachFiles/cultureInfoCourt/monthServ/1702513157312.jpg";
+        } else if (i %10==2) {
+            show.imageObject = "https://www.mcst.go.kr/attachFiles/cultureInfoCourt/monthServ/1702442615283.jpg";
+        } else if (i %10==3) {
+            show.imageObject = "https://www.mcst.go.kr/attachFiles/cultureInfoCourt/monthServ/1702427175230.jpg";
+        } else if (i %10==4){
+            show.imageObject ="https://www.mcst.go.kr/attachFiles/cultureInfoCourt/monthServ/1702514099694.gif";
+        } else if (i %10==5){
+            show.imageObject ="https://www.mcst.go.kr/attachFiles/cultureInfoCourt/monthServ/1702514134298.jpg";
+        } else if (i %10==6){
+            show.imageObject ="https://www.mcst.go.kr/attachFiles/cultureInfoCourt/monthServ/1699417551991.jpg";
+        } else if (i %10==7){
+            show.imageObject ="https://www.mcst.go.kr/attachFiles/cultureInfoCourt/monthServ/1701653101315.jpg";
+        } else if (i %10==8){
+            show.imageObject ="https://www.mcst.go.kr/attachFiles/cultureInfoCourt/monthServ/1701739949828.jpg";
+        } else if(i %10==9){
+            show.imageObject ="https://www.mcst.go.kr/attachFiles/cultureInfoCourt/monthServ/1702267476272.jpg";
+        } else{
+            show.imageObject ="https://www.mcst.go.kr/attachFiles/cultureInfoCourt/monthServ/1701848885467.jpg";
+        }
+        show.description = "description";
+        show.viewCount = "viewCount";
+        return show;
     }
 
 }
